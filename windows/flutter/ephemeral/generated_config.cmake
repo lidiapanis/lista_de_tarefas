672e12d0cb1070519395f2d0ad99688b4b805212
Lidia Panis <lpse@live.com>
2023-06-15 09:46:35 -0300

    Add files via upload

--- windows/flutter/ephemeral/generated_config.cmake
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -1,6 +1,6 @@
 # Generated code do not commit.
 file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "C:\\Users\\lidiae\\Desktop\\lista_simples\\Nova pasta\\lista_de_tarefas\\listatarefas" PROJECT_DIR)
+file(TO_CMAKE_PATH "C:\\Users\\lidiae\\Desktop\\lista_de_tarefas-main" PROJECT_DIR)
 
 set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
 set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
@@ -11,14 +11,14 @@ set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
   "FLUTTER_ROOT=C:\\src\\flutter"
-  "PROJECT_DIR=C:\\Users\\lidiae\\Desktop\\lista_simples\\Nova pasta\\lista_de_tarefas\\listatarefas"
+  "PROJECT_DIR=C:\\Users\\lidiae\\Desktop\\lista_de_tarefas-main"
   "FLUTTER_ROOT=C:\\src\\flutter"
-  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\lidiae\\Desktop\\lista_simples\\Nova pasta\\lista_de_tarefas\\listatarefas\\windows\\flutter\\ephemeral"
-  "PROJECT_DIR=C:\\Users\\lidiae\\Desktop\\lista_simples\\Nova pasta\\lista_de_tarefas\\listatarefas"
-  "FLUTTER_TARGET=C:\\Users\\lidiae\\Desktop\\lista_simples\\Nova pasta\\lista_de_tarefas\\listatarefas\\lib\\screens\\main.dart"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\lidiae\\Desktop\\lista_de_tarefas-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\lidiae\\Desktop\\lista_de_tarefas-main"
+  "FLUTTER_TARGET=C:\\Users\\lidiae\\Desktop\\lista_de_tarefas-main\\lib\\main.dart"
   "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=false"
-  "PACKAGE_CONFIG=C:\\Users\\lidiae\\Desktop\\lista_simples\\Nova pasta\\lista_de_tarefas\\listatarefas\\.dart_tool\\package_config.json"
+  "PACKAGE_CONFIG=C:\\Users\\lidiae\\Desktop\\lista_de_tarefas-main\\.dart_tool\\package_config.json"
 )
